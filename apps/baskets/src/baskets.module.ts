c25679f04414abf3d5cf1dfe01610b82dd78ae31
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-10-19 01:10:39 +0300

    init daskets module

--- apps/baskets/src/baskets.module.ts
+++ apps/baskets/src/baskets.module.ts
@@ -1,7 +1,44 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { getEnvironmentFile } from '@app/common';
+
 import { CoreModule } from './core/core.module';
 
+const envFilePath = `./apps/baskets/${getEnvironmentFile(process.env.NODE_ENV)}`;
+const DefinitionConfigModule = ConfigModule.forRoot({
+  envFilePath: envFilePath,
+  isGlobal: true,
+});
+
+const DefinitionGraphQLModule = GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+  driver: ApolloFederationDriver,
+  autoSchemaFile: {
+    federation: 2,
+  },
+});
+
+const DefinitionTypeOrmModule = TypeOrmModule.forRootAsync({
+  imports: [ConfigModule],
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    host: config.get('DB_HOST'),
+    port: config.get<number>('DB_PORT'),
+    username: config.get('DB_USERNAME'),
+    password: config.get('DB_PASSWORD'),
+    database: config.get('DB_NAME'),
+    synchronize: true,
+    autoLoadEntities: true,
+    logging: true,
+  }),
+
+  inject: [ConfigService],
+});
+
 @Module({
-  imports: [CoreModule],
+  imports: [DefinitionConfigModule, DefinitionGraphQLModule, DefinitionTypeOrmModule, CoreModule],
 })
 export class BasketsModule {}
