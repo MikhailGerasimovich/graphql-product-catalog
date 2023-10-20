cf1ebd0523916b9b15424e7034395649e7de86b4
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-10-20 10:13:32 +0300

    add getBasket, takeProduct and putProduct methods

--- apps/baskets/src/core/baskets/basket.service.ts
+++ apps/baskets/src/core/baskets/basket.service.ts
@@ -2,11 +2,33 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { Payload } from '@app/common';
+
 import { Basket } from './entities';
 
 @Injectable()
 export class BasketService {
   constructor(@InjectRepository(Basket) private readonly basketRepository: Repository<Basket>) {}
+
+  async getBasket(payload: Payload): Promise<Basket> {
+    const userId = 4;
+
+    const basket = await this.basketRepository.findOne({
+      where: { userId: userId },
+      relations: ['basketProducts'],
+    });
+
+    return basket;
+  }
+
+  async takeProduct(productId: number, payload: Payload): Promise<Basket> {
+    return null;
+  }
+
+  async putProduct(productId: number, payload: Payload): Promise<Basket> {
+    return null;
+  }
+
   async forUser(userId: number): Promise<Basket> {
     const basket = await this.basketRepository.findOne({
       where: { userId },
