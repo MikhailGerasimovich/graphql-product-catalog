a8ba8af166c24463d2f0261fab72e7514e106d4d
Bear_999_ <mihailgerasimovich2002@gmail.com>
2023-10-20 23:37:08 +0300

    change command repository

--- apps/catalogs/src/core/products/infrastructure/command-product.repository.ts
+++ apps/catalogs/src/core/products/infrastructure/command-product.repository.ts
@@ -17,11 +17,13 @@ export class CommandProductRepository {
     const product = await this.repository.findOne({ where: { id } });
     return product;
   }
+
   async create(createProductInput: CreateProductInput): Promise<CommandProduct> {
     const productEntity = this.repository.create(createProductInput);
     const savedProduct = await this.repository.save(productEntity);
     return savedProduct;
   }
+
   async update(id: number, updateProductInput: UpdateProductInput): Promise<CommandProduct> {
     const existingProduct = await this.findOne(id);
     const productEntity = this.repository.create(updateProductInput);
@@ -31,6 +33,7 @@ export class CommandProductRepository {
     });
     return updatedProduct;
   }
+
   async delete(id: number): Promise<boolean> {
     const data = await this.repository.delete(id);
     return data && data.affected > 0;
